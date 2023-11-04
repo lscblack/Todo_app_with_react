5ce66ecebd5200c120d034edaa3a6caf06fb00cd
lscblack <l.christian@alustudent.com>
2023-11-04 23:18:11 +0200

    finish all

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import styles from '../style.module.css'
  const Header = () => {
 	return(
-			<h3 className={styles.header}>Todo App.</h3>
+			<h3 className={styles.header}>Todo App .</h3>
 		)
 }
 export default Header
